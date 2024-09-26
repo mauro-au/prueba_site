b7128d962f2fa4b686f476de93b8549f7b1897d0
mauro-au <mauricio.a.developer@gmail.com>
2024-09-26 12:13:37 -0300

    se agrega gema fake y datos en el seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+15.times do |i|
+  @character = Character.create(name: Faker::Movies::HarryPotter.character, location: Faker::Movies::HarryPotter.location , house: Faker::Movies::HarryPotter.house )
+end
